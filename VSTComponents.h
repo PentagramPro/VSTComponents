8c4f3ef64eacb82caa1014583bdd301a5ea01712
Max <realevilhq@gmail.com>
2018-10-21 03:54:31 +0200

    first header file

---
+++ VSTComponents.h
@@ -0,0 +1,23 @@
+
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.txt file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:                 VSTComponents
+  vendor:             PentagramPro
+  version:            1.0.0
+  name:               VSTComponents
+  description:        VST stuff
+  license:            GNU v3
+  minimumCppStandard: 14
+
+  dependencies:       juce_audio_basics, juce_audio_formats
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
